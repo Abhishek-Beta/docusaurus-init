db5c377f138534336f681b5b7f336dbd4a564884
Abhishek <abhishek@betalectic.com>
2024-10-10 15:57:19 +0530

    Added h2 text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,12 +17,7 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+          <h2>H2 From PR Preview</h2>
         </div>
       </div>
     </header>
