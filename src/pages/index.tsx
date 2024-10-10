d36ca57e339abf00be8b3de5fef41174aabcde74
Abhishek <abhishek@betalectic.com>
2024-10-10 19:02:35 +0530

    changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
-            Docusaurus Tutorial - 5min ⏱️
+            Docusaurus Tutorial - 2min ⏱️
           </Link>
         </div>
       </div>
