075944a3dc97458a85154864a3282f022d7d3ae0
Abhishek <abhishek@betalectic.com>
2024-10-10 12:11:25 +0530

    Changed baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://Abhishek-Beta.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/docusaurus-init/",
+  baseUrl: process.env.BASE_URL || "/docusaurus-init/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
