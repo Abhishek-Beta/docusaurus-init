dc9e78023384f1eb749e8fd1b655436224c4ca2a
Abhishek <abhishek@betalectic.com>
2024-10-10 12:26:11 +0530

    Added dynamic baseUrl in config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,6 +2,8 @@ import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
+const isPreview = process.env.DEPLOY_ENV === "preview";
+
 const config: Config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
@@ -11,7 +13,9 @@ const config: Config = {
   url: "https://Abhishek-Beta.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.BASE_URL || "/docusaurus-init/",
+  baseUrl: isPreview
+    ? `/docusaurus-init/pr-preview/${process.env.PR_NUMBER}/`
+    : "/docusaurus-init/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
